b170cbd8ab885e6ace87e4f1b66272031dc95459
Victor502 <victorahorton@gmail.com>
2020-07-13 18:23:07 -0400

    added firebase auth and auth screens

---
+++ src/auth/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Input} from 'react-native';
+
+function SignUp(Prop) {
+  return (
+    <View>
+      <Text>Sign in Page</Text>
+    </View>
+  );
+}
+
+export default SignUp;
