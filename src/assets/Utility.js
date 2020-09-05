c9f4410caeb441516eb88548c466ac82d5ca2a1a
Victor502 <victorahorton@gmail.com>
2020-09-04 20:43:40 -0400

    added async storgae, utility file

--- src/assets/Utility.js
+++ src/assets/Utility.js
@@ -1,5 +1,7 @@
 import 'react-native-get-random-values'
 import {v4 as uuidv4} from "uuid";
+import AsyncStorage from '@react-native-community/async-storage';
+import config from './config/config.js';
 
 Utility = {
   DeleteFromServer: async (endpoint, data) => {
